a514b7748bc1208d4b6d3a9d96075fc610805092
Simon-Oliver <stauffer.simon@outlook.com>
2022-11-02 11:29:15 +1100

    Added escape sequence to cleare console after choosing menu item to stop duplicating console output

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <ios>
+#include <chrono>
+#include <thread>
 
 int main()
 {
@@ -25,20 +28,22 @@ int main()
     {
         std::cout << "Something went wrong!!!" << std::endl;
     };
-
     menuFile.close();
-
     menu(menuItems);
-
     while (isRunning)
     {
+        // Clear the visible area of the console using escape sequences
+        std::cout << "\e[H\e[0J" << std::flush;
+        // Display the menu
+        menu(menuItems);
         // Get user input and save it as choice
-        int choice = getInput("Please choose and option from the menu: ", menuItems.size());
+        int choice;
         // Print the choice in the terminal
         if (choice == menuItems.size())
         {
             isRunning = false;
             std::cout << "\nApplication is closing." << std::endl;
+            break;
         };
 
         switch (choice)
@@ -49,9 +54,10 @@ int main()
         case 4:
             getItems();
         default:
-            std::cout << "You choose " + menuItems[choice - 1] << "\r" << std::endl;
+            // std::cout << "You choose " + menuItems[choice - 1] << "\r" << std::endl;
             break;
         }
+        choice = getInput("Please choose and option from the menu: ", menuItems.size());
     };
     return 0;
 };
